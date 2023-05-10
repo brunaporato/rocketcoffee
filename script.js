cb526b6b1cccdc7f79e39fe6ae14dc54ad0da4b5
Bruna Porato <brunaporato@gmail.com>
2023-05-10 08:32:10 -0300

    menu burger

---
+++ script.js
@@ -0,0 +1,12 @@
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const menuBurger = document.querySelector(".menu-burger");
+
+function burger() {
+  openMenu.classList.toggle('hide');
+  menuBurger.classList.toggle('hide');
+  closeMenu.classList.toggle('hide');
+};
+
+openMenu.onclick = burger
+closeMenu.onclick = burger
