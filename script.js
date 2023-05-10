59876acbb624443f874e94c9d940ceefad240b44
Bruna Porato <brunaporato@gmail.com>
2023-05-10 08:35:15 -0300

    menu burger fix

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const openMenu = document.querySelector(".open-menu");
 const closeMenu = document.querySelector(".close-menu");
 const menuBurger = document.querySelector(".menu-burger");
+const imgCoffee = document.querySelector("#coffee")
 
 function burger() {
   openMenu.classList.toggle('hide');
   menuBurger.classList.toggle('hide');
   closeMenu.classList.toggle('hide');
+  imgCoffee.classList.toggle('hide');
 };
 
 openMenu.onclick = burger
